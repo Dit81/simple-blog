5170ddff35243a9a15b3db7023c8cf583f1bd51c
pyhale <spielga@gmail.com>
2014-08-27 09:53:36 +0800

    add ckeditor to post write page

--- application/views/post_write.php
+++ application/views/post_write.php
@@ -1,16 +1,32 @@
 <html>
-<head><title>Write Post</title></head>
+
+<head>
+
+<title>Write Post</title>
+
+<script type="text/javascript" src="<?php echo base_url().'resource/ckeditor/ckeditor.js';?>"></script>
+
+<script type="text/javascript" src="<?php echo base_url().'resource/textjsurl.js';?>"></script>
+
+</head>
 <body>
+
+<script type="text/javascript">
+text();
+</script>
 
 <?php echo validation_errors();?>
 
 <?php echo form_open('blog/write') ?>
 
 <input type="text" name="title" /><br>
-<textarea name="body" cols="45" row="5"></textarea><br>
+<textarea name="body" cols="45" row="5" class="ckeditor"></textarea>
+<br>
 <input type="submit" value="submit" />
 
 </form>
+
+<script type="text/javascript">CKEDITOR.replace('body',{toolbar:'Full',skin:'moono'});</script>
 
 </body>
 </html>
